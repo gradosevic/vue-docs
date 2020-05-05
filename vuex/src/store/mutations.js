c792d098d9913cb10170f99b08a1fe5fc61af2be
Goran Radosevic <goran.radosevic@outlook.com>
2020-05-05 21:01:13 +0200

    Update mutations.js

--- vuex/src/store/mutations.js
+++ vuex/src/store/mutations.js
@@ -1,3 +1,7 @@
-export const addNumber = (state, number) => {
+export const addNumber = (state, number) => { //second argument should always be one payload
   state.numbers.push(number)
 }
+export const addNumber2 = (state, {min, max}) => { //object destructuring
+  state.numbers.push(Math.floor(Math.random() * (max - min)) + min)
+}
+
