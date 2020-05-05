20383e1d32f6f297127d4e300de6a68503e5de16
Goran Radosevic <goran.radosevic@outlook.com>
2020-05-05 19:30:52 +0200

    Update getters.js

--- vuex/src/store/getters.js
+++ vuex/src/store/getters.js
@@ -2,3 +2,6 @@ export const number = (state) => {
   return state.number
 }
 export const numbers = (state) => state.numbers
+export const sum = (state) => {
+  return state.numbers.reduce((a, b) => a + b, 0)
+}
