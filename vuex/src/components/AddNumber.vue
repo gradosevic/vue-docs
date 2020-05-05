c3088916cd83449053dc73c0c9381de8f661dfda
Goran Radosevic <goran.radosevic@outlook.com>
2020-05-05 19:50:41 +0200

    Update AddNumber.vue

--- vuex/src/components/AddNumber.vue
+++ vuex/src/components/AddNumber.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <a href="#">Add Random Number</a>
+    <a href="#" @click.prevent="add">Add Random Number</a>
   </div>
 </template>
 <script>
 export default{
-  name: 'add-number'
+  name: 'add-number',
+  methods:{
+    add(){
+      this.$store.state.numbers.push(3)
+    }
+  }
 }
 </script>
