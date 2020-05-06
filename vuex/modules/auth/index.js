78d9bfaa5c81fbedbba9d947dc227d50ac9e50ac
Goran Radosevic <goran.radosevic@outlook.com>
2020-05-06 13:45:46 +0200

    Update index.js

--- vuex/modules/auth/index.js
+++ vuex/modules/auth/index.js
@@ -1,1 +1,12 @@
+import state from './state'
+import * from './getters'
+import * from './mutations'
+import * from './actions'
 
+export default{
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
