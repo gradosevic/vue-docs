c7488a37500a7120adff4972e6a7d898a922478a
Goran Radosevic <goran.radosevic@outlook.com>
2020-05-06 11:19:46 +0200

    Create Spreading.vue

---
+++ vuex/src/components/Spreading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+import { mapActions } from 'vuex'
+export default{
+  name: 'add-number',
+  methods:{
+    ...mapActions({
+      addProduct: 'addProduct'
+    })
+  }
+}
+</script>
